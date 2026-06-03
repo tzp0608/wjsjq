9041daef620bea3b7747749a3acf2514ddcc0266
云舟新mac <yunzhouxinmac@yunzhouxinmacdeMacBook-Pro.local>
2026-06-03 14:42:31 +0800

    fix: register BaiduPanController in BaiduPanModule

--- pan-receiver/backend/src/baidu-pan/baidu-pan.module.ts
+++ pan-receiver/backend/src/baidu-pan/baidu-pan.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { BaiduPanService } from './baidu-pan.service';
+import { BaiduPanController } from './baidu-pan.controller';
 import { PrismaModule } from '../prisma/prisma.module';
 import { CommonModule } from '../common/common.module';
 
 @Module({
   imports: [PrismaModule, CommonModule],
   providers: [BaiduPanService],
+  controllers: [BaiduPanController],
   exports: [BaiduPanService],
 })
 export class BaiduPanModule {}
